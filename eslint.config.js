ca1be88f6f7fd07723ffcb1fd7aac232cb266786
pinstinct <contact@limhm.dev>
2026-02-09 16:04:23 +0900

    Add frontend pre-commit lint security and format hooks

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import globals from "globals";
+import security from "eslint-plugin-security";
+
+export default [
+  {
+    ignores: ["node_modules/**", "data/**", "dist/**"],
+  },
+  js.configs.recommended,
+  security.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2022,
+        Chart: "readonly",
+        WKBLDatabase: "readonly",
+        initSqlJs: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
